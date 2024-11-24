37d0a30cccbb16bcae7671a9e08a7702f59cdbc3
Omar-A-96 <o.abdelkader1996@gmail.com>
2024-11-24 15:42:26 +0200

    م

--- src/Components/buttom-home-section3/buttom-home-section3.jsx
+++ src/Components/buttom-home-section3/buttom-home-section3.jsx
@@ -3,8 +3,7 @@ import './buttom-home-section3.css'
 export default function Btnsection3(){
     return(
         <div className="btn-section3 col-md-12 col-lg-6">
-            <a href="#">
-                <span><BiRightTopArrowCircle /></span>Go shopping now
+            <a href="#"><span><BiRightTopArrowCircle /></span>Go shopping now
             </a>
         </div>
     )
